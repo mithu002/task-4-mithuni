7ce505b6f6191be2cbbcb6eef3202337a3ca1664
mithu002 <106811536+mithu002@users.noreply.github.com>
2022-06-21 22:45:44 +0530

    Update Ref02.java

--- task4/src/KUnit2/Ref02.java
+++ task4/src/KUnit2/Ref02.java
@@ -2,7 +2,7 @@ package KUnit2;
 
 public class Ref02 {
 
-	public static void main(String[] rgs) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		SimpleM sm = new SimpleM();
